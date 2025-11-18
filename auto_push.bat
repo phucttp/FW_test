f04b856ab04137bfb9adb27bde9e684087684496
phucttp <trantrongphucttp27@gmail.com>
2025-11-18 22:47:22 +0700

    Auto update via FlashPorter Tool

--- auto_push.bat
+++ auto_push.bat
@@ -1,27 +1,28 @@
 @echo off
-title AUTO PUSH FIRMWARE TO GIT
-color 0A
-echo ==========================================
-echo      DANG DAY CODE LEN GITHUB...
-echo ==========================================
-echo Repo: https://github.com/phucttp/FW_Lib.git
-echo.
-cd /d "C:\Users\DELL\workspace\ESP32\toolAddFirmware\_release_for_git"
-if not exist .git (
-    echo [Init Git]...
-    git init
-    git branch -M main
-)
-echo [Add Files]...
-git add .
-echo [Commit]...
-git commit -m "Auto update via FlashPorter Tool"
-echo [Remote Config]...
-git remote remove origin >nul 2>&1
-git remote add origin https://github.com/phucttp/FW_Lib.git
-echo [Pushing]...
-echo (Neu la lan dau, Windows se hien popup dang nhap)
-git push -u origin main --force
-echo.
-echo HOAN TAT!
-pause
+                        title AUTO PUSH FIRMWARE TO GIT
+                        color 0A
+                        echo ==========================================
+                        echo      DANG DAY CODE LEN GITHUB...
+                        echo ==========================================
+                        echo Repo: https://github.com/phucttp/FW_Lib.git
+                        echo.
+                        cd /d "C:\Users\DELL\workspace\ESP32\toolAddFirmware\_release_for_git"
+                        if not exist .git (
+                            echo [Init Git]...
+                            git init
+                            git branch -M main
+                        )
+                        echo [Add Files]...
+                        git add -A
+                        echo [Commit]...
+                        git commit -m "Auto update via FlashPorter Tool"
+                        echo [Remote Config]...
+                        git remote remove origin >nul 2>&1
+                        git remote add origin https://github.com/phucttp/FW_Lib.git
+                        echo [Pushing]...
+                        echo (Neu la lan dau, Windows se hien popup dang nhap)
+                        git push -u origin main --force
+                        echo.
+                        echo HOAN TAT!
+                        pause
+                        
