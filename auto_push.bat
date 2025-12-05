aa6deb0d7b4ee204bd0ef9022c41498e32e278f8
phucttp <trantrongphucttp27@gmail.com>
2025-12-06 00:19:06 +0700

    Auto update via FlashPorter Tool

---
+++ auto_push.bat
@@ -0,0 +1,28 @@
+@echo off
+                        title AUTO PUSH FIRMWARE TO GIT
+                        color 0A
+                        echo ==========================================
+                        echo      DANG DAY CODE LEN GITHUB...
+                        echo ==========================================
+                        echo Repo: https://PPhucNAME@github.com/PPhucNAME/hehe
+                        echo.
+                        cd /d "C:\Users\DELL\workspace\IDF\ESP_MUL\Muti\toolAddFirmware\dist\FlashPorter\_release_for_git"
+                        if not exist .git (
+                            echo [Init Git]...
+                            git init
+                            git branch -M main
+                        )
+                        echo [Add Files]...
+                        git add -A
+                        echo [Commit]...
+                        git commit -m "Auto update via FlashPorter Tool"
+                        echo [Remote Config]...
+                        git remote remove origin >nul 2>&1
+                        git remote add origin https://PPhucNAME@github.com/PPhucNAME/hehe
+                        echo [Pushing]...
+                        echo (Neu la lan dau, Windows se hien popup dang nhap)
+                        git push -u origin main --force
+                        echo.
+                        echo HOAN TAT!
+                        pause
+                        
